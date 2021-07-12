1a0d6850e9e90ed38e33ee1c7163c1b9217ba631
HOMMA Takashi <homma@daniel-soft.com>
2021-07-13 06:36:24 +0900

    Specification追加

---
+++ jr-pricing/Model/Specification/DepartureDate.vb
@@ -0,0 +1,15 @@
+﻿Imports Fhi.Fw.Domain
+
+Namespace Model.Specification
+    ''' <summary>
+    ''' 出発日
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class DepartureDate : Inherits PrimitiveValueObject(Of DateTime?)
+
+        Public Sub New(value As Date?)
+            MyBase.New(value)
+        End Sub
+
+    End Class
+End Namespace
